bff646c4570c370183b1fb4ea0e509278c053dbf
Labzunova <dasha.labzunova@mail.ru>
2021-05-26 04:01:50 +0300

    add useEffect

--- src/api/api.js
+++ src/api/api.js
@@ -6,7 +6,7 @@ const instance = axios.create({
     baseURL: "https://social-network.samuraijs.com/api/1.0/",
     withCredentials: true,
     headers: {
-        "API-KEY": "8d5bcc0f-d8f0-4670-8b16-b269f20643b5"
+        "API-KEY": "ebeea3ac-37ee-4ff0-9f72-7333376d1297"
     }
 })
 
