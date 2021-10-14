4b623b4233389a1a245a69a48f7a7a9d330901db
vlad <vlad.volodin.01@mail.ru>
2021-10-15 02:28:34 +0300

    Updates

---
+++ static/js/4.320720d1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpvladek=this.webpackJsonpvladek||[]).push([[4],{357:function(e,s,t){e.exports={item:"Music_item__2ushy"}},364:function(e,s,t){"use strict";t.r(s);var i=t(357),c=t.n(i),n=t(1);s.default=function(e){return Object(n.jsx)("div",{className:c.a.item,children:"Music"})}}}]);
+//# sourceMappingURL=4.320720d1.chunk.js.map
